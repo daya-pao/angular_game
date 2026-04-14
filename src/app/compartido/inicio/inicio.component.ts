606b4ba25c107bacc9f68d952ecbec79bf70c0f6
daya-pao <dayana06052002@gmail.com>
2026-04-14 18:10:53 -0500

    feat: mejora de diseño responsivo y página 404 gamer

--- src/app/compartido/inicio/inicio.component.ts
+++ src/app/compartido/inicio/inicio.component.ts
@@ -11,12 +11,12 @@ import { FooterComponent } from '../footer/footer.component';
 export class InicioComponent {
 
   public juegos = [
-    {portada:'assets/img/g1.jpg',titulo:'juego lorem'},
-    {portada:'assets/img/g2.jpg',titulo:'juego lorem'},
-    {portada:'assets/img/g3.jpg',titulo:'juego lorem'},
-    {portada:'assets/img/g4.jpg',titulo:'juego lorem'},
-    {portada:'assets/img/g5.jpg',titulo:'juego lorem'},
-    {portada:'assets/img/g7.jpg',titulo:'juego lorem'},
+    {portada:'assets/img/g1.jpg',titulo:'Call of Duty' , precio:'300 '},
+    {portada:'assets/img/g2.jpg',titulo:'Battlefield',precio:'500'},
+    {portada:'assets/img/g3.jpg',titulo:'Hallo reach',precio:'200'},
+    {portada:'assets/img/g4.jpg',titulo:'Fortnite', precio:'100'},
+    {portada:'assets/img/g5.jpg',titulo:'CRT Crash',precio:'600'},
+    {portada:'assets/img/g7.jpg',titulo:'The Surge',precio:'400'},
   ]
 
 
